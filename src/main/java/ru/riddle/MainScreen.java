c5105ae95f6bcebc5e0740353b01fdf477995121
Elkin Lev <elkinlev2008@gmail.com>
2024-03-29 22:10:31 +0300

    Adding createContent method

--- src/main/java/ru/riddle/MainScreen.java
+++ src/main/java/ru/riddle/MainScreen.java
@@ -1,6 +1,7 @@
 package ru.riddle;
 
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
@@ -8,15 +9,16 @@ import javafx.stage.Stage;
 
 public class MainScreen extends Application {
 
+    private Parent createContent() {
+
+        Label l = new Label("Hello, JavaFX");
+
+        return new StackPane(l);
+    }
 
     @Override
     public void start(Stage stage){
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        Scene scene = new Scene(createContent(), 640, 480);
 
         stage.setTitle("Physics Simulator of Surface Tension");
         stage.setScene(scene);
