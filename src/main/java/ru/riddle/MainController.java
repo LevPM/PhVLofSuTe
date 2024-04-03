6357d7640756ef573fa00fe8d7b43054909e1180
Elkin Lev <elkinlev2008@gmail.com>
2024-04-03 13:28:59 +0300

    Adding MainScreenFXML.fxml and First GUI

---
+++ src/main/java/ru/riddle/MainController.java
@@ -0,0 +1,6 @@
+package ru.riddle;
+
+public class MainController{
+
+
+}
--- src/main/java/ru/riddle/MainScreen.java
+++ src/main/java/ru/riddle/MainScreen.java
@@ -1,28 +1,32 @@
 package ru.riddle;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainScreen extends Application {
 
-    private Parent createContent() {
-
-        Label l = new Label("Hello, JavaFX");
-
-        return new StackPane(l);
-    }
-
     @Override
-    public void start(Stage stage){
-        Scene scene = new Scene(createContent(), 640, 480);
+    public void start(Stage primaryStage){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/MainScreenFXML.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
 
-        stage.setTitle("Physics Simulator of Surface Tension");
-        stage.setScene(scene);
-        stage.show();
+            primaryStage.setScene(scene);
+
+            MainController controller = loader.getController();
+
+            primaryStage.setTitle("PhSofSuTe");
+
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
