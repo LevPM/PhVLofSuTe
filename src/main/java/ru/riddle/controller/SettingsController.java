19c601fe34c3fb2b3906f647ba34d01ece2b636b
Elkin Lev <elkinlev2008@gmail.com>
2024-04-03 16:56:01 +0300

    Correcting SettingsScreenFXML.fxml and adding BackButton action handling

--- src/main/java/ru/riddle/controller/SettingsController.java
+++ src/main/java/ru/riddle/controller/SettingsController.java
@@ -1,10 +1,21 @@
 package ru.riddle.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import ru.riddle.model.SettingsScreenModel;
 
 public class SettingsController {
 
+    private final SettingsScreenModel model;
+
     @FXML
     private ListView listView;
+
+    public SettingsController(){
+        model = new SettingsScreenModel();
+    }
+    public void handleBackButtonAction(ActionEvent event){
+        model.openMainScreen(event);
+    }
 }
