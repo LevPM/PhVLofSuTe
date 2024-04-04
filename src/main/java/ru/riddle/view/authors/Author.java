5ecd5018ea97e68471dec21ff98e3d3340a2cbe7
Elkin Lev <elkinlev2008@gmail.com>
2024-04-04 16:34:10 +0300

    Adding toString method to Author class

--- src/main/java/ru/riddle/view/authors/Author.java
+++ src/main/java/ru/riddle/view/authors/Author.java
@@ -1,4 +1,9 @@
 package ru.riddle.view.authors;
 
 public record Author(String name, String nickname, String position) {
+
+    @Override
+    public String toString(){
+        return name + " (" + nickname + ")" + " - " + position;
+    }
 }
