622890f468ed56702b6993d85fe6bd5df17391d5
Elkin Lev <elkinlev2008@gmail.com>
2024-04-05 18:21:49 +0300

    Adding initializing model in MainController's constructor

--- src/main/java/ru/riddle/controller/MainController.java
+++ src/main/java/ru/riddle/controller/MainController.java
@@ -1,4 +1,12 @@
 package ru.riddle.controller;
 
+import ru.riddle.model.menu.MainModel;
+
 public class MainController {
+
+    private final MainModel model;
+
+    public MainController() {
+        model = new MainModel();
+    }
 }
