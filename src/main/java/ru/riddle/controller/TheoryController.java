c0314c1d354377ec0e48be137063aa2c27ea1d7d
Elkin Lev <elkinlev2008@gmail.com>
2024-04-08 22:17:38 +0300

    Adding LeaveButton and button actions handling

--- src/main/java/ru/riddle/controller/TheoryController.java
+++ src/main/java/ru/riddle/controller/TheoryController.java
@@ -1,13 +1,21 @@
 package ru.riddle.controller;
 
+import javafx.event.ActionEvent;
 import ru.riddle.model.TheoryModel;
 
 public class TheoryController {
 
-    private TheoryModel model;
+    private final TheoryModel model;
 
     public TheoryController(){
         model = new TheoryModel();
     }
 
+    public void handleLeaveButtonAction(ActionEvent event) {
+        model.leaveTheory(event);
+    }
+
+    public void handleNextButtonAction(ActionEvent event) {
+
+    }
 }
--- src/main/java/ru/riddle/model/TheoryModel.java
+++ src/main/java/ru/riddle/model/TheoryModel.java
@@ -1,4 +1,33 @@
 package ru.riddle.model;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class TheoryModel {
+
+    public void leaveTheory(ActionEvent event){
+        Parent root = null;
+        try{
+            root = FXMLLoader.load(getClass().getResource("/view/MenuFormulasScreenFXML.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        double height = stage.getHeight();
+        double width = stage.getWidth();
+        boolean isFullScreen = stage.isFullScreen();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setHeight(height);
+        stage.setWidth(width);
+        stage.setFullScreen(isFullScreen);
+        stage.show();
+    }
 }
