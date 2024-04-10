93183dc30259c49ddfa1d770b853798afe9f04c8
Elkin Lev <elkinlev2008@gmail.com>
2024-04-10 16:26:45 +0300

    Changing TheoryFirstExamples controller from TheoryController to TheoryExamplesController

--- src/main/java/ru/riddle/controller/TheoryExamplesController.java
+++ src/main/java/ru/riddle/controller/TheoryExamplesController.java
@@ -1,4 +1,25 @@
 package ru.riddle.controller;
 
+import javafx.event.ActionEvent;
+import ru.riddle.model.theory.TheoryExamplesModel;
+
 public class TheoryExamplesController {
+
+    private TheoryExamplesModel model;
+
+    public TheoryExamplesController() {
+        this.model = new TheoryExamplesModel();
+    }
+
+    public void handleNextButtonAction(ActionEvent event) {
+
+    }
+
+    public void handlePreviousButtonAction(ActionEvent event) {
+
+    }
+
+    public void handleLeaveButtonAction(ActionEvent event) {
+
+    }
 }
