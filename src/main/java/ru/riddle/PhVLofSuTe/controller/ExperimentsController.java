b8267116f66f5fd3c1666d8352e821864b1058e6
Elkin Lev <elkinlev2008@gmail.com>
2024-04-21 16:51:03 +0300

    Adding Experiments model and controller

---
+++ src/main/java/ru/riddle/PhVLofSuTe/controller/ExperimentsController.java
@@ -0,0 +1,12 @@
+package ru.riddle.PhVLofSuTe.controller;
+
+import ru.riddle.PhVLofSuTe.model.experiments.ExperimentsModel;
+
+public class ExperimentsController {
+
+    private final ExperimentsModel model;
+
+    public ExperimentsController() {
+        this.model = new ExperimentsModel();
+    }
+}
---
+++ src/main/java/ru/riddle/PhVLofSuTe/model/experiments/ExperimentsModel.java
@@ -0,0 +1,4 @@
+package ru.riddle.PhVLofSuTe.model.experiments;
+
+public class ExperimentsModel {
+}
