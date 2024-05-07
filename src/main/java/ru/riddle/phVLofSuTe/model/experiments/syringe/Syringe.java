858d62d92d51327fb234946c047f5df66ff2dcd7
Elkin Lev <elkinlev2008@gmail.com>
2024-05-07 19:58:17 +0300

    Removing loadFXML method, removing fields

--- src/main/java/ru/riddle/phVLofSuTe/model/experiments/syringe/Syringe.java
+++ src/main/java/ru/riddle/phVLofSuTe/model/experiments/syringe/Syringe.java
@@ -1,46 +1,20 @@
 package ru.riddle.phVLofSuTe.model.experiments.syringe;
 
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.layout.AnchorPane;
-
-import java.io.IOException;
+import ru.riddle.phVLofSuTe.model.util.FXMLs;
+import ru.riddle.phVLofSuTe.model.util.ModelUtil;
 
 public class Syringe extends AnchorPane{
-
-    @FXML
-    private Group syringeTip;
-
-    @FXML
-    private Group syringePiston;
-
-    @FXML
-    private AnchorPane syringe;
-
-    @FXML
-    private Group syringePart;
 
     private int countOfParts = 6; //default count of parts = 6
 
     public Syringe(){
-        loadFXML();
+        ModelUtil.downloadCustomComponentFXML(FXMLs.SYRINGE, this);
     }
 
     public Syringe(int countOfParts){
         this();
         this.countOfParts = countOfParts;
-    }
-
-    private void loadFXML(){
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/experiments/syringe/syringeFXML.fxml"));
-        loader.setRoot(this);
-
-        try{
-            loader.load();
-        } catch(IOException e){
-            e.printStackTrace();
-        }
     }
 
     private void buildSyringe(){
