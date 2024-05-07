3d59b39889914b1a5c63476f142ac4b06540b551
Elkin Lev <elkinlev2008@gmail.com>
2024-05-07 16:10:00 +0300

    Adding loadFXML method to Syringe, adding new constructor, starting adding buildSyringe method

--- src/main/java/ru/riddle/phVLofSuTe/model/experiments/Syringe.java
+++ src/main/java/ru/riddle/phVLofSuTe/model/experiments/Syringe.java
@@ -2,6 +2,7 @@ package ru.riddle.phVLofSuTe.model.experiments;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
@@ -11,15 +12,34 @@ public class Syringe extends AnchorPane{
     @FXML
     private AnchorPane syringe;
 
+    @FXML
+    private Group syringePart;
+
+    private int countOfParts = 6; //default count of parts = 6
+
     public Syringe(){
+        loadFXML();
+    }
+
+    public Syringe(int countOfParts){
+        this();
+        this.countOfParts = countOfParts;
+    }
+
+    private void loadFXML(){
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/experiments/syringe/syringeFXML.fxml"));
         loader.setRoot(this);
-        loader.setController(this);
 
         try{
             loader.load();
         } catch(IOException e){
             e.printStackTrace();
+        }
+    }
+
+    private void buildSyringe(){
+        for(int i = countOfParts; i > 0; i--){
+
         }
     }
 }
