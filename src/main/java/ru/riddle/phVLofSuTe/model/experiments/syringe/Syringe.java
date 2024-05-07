c27e9b82c9efef3adbe0a6a0632e4706b924c5b0
Elkin Lev <elkinlev2008@gmail.com>
2024-05-07 20:14:37 +0300

    Adding syringe field, removing filling from AnchorPane in the syringeFXML

--- src/main/java/ru/riddle/phVLofSuTe/model/experiments/syringe/Syringe.java
+++ src/main/java/ru/riddle/phVLofSuTe/model/experiments/syringe/Syringe.java
@@ -1,10 +1,14 @@
 package ru.riddle.phVLofSuTe.model.experiments.syringe;
 
+import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 import ru.riddle.phVLofSuTe.model.util.FXMLs;
 import ru.riddle.phVLofSuTe.model.util.ModelUtil;
 
 public class Syringe extends AnchorPane{
+
+    @FXML
+    private AnchorPane syringe;
 
     private int countOfParts = 6; //default count of parts = 6
 
