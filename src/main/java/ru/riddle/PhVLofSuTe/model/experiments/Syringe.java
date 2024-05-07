b2bf31fbb28ed03655c5771607d91de682c5ae82
Elkin Lev <elkinlev2008@gmail.com>
2024-05-07 15:46:16 +0300

    Creating Syringe FXML component

---
+++ src/main/java/ru/riddle/PhVLofSuTe/model/experiments/Syringe.java
@@ -0,0 +1,25 @@
+package ru.riddle.PhVLofSuTe.model.experiments;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+public class Syringe extends AnchorPane{
+
+    @FXML
+    private AnchorPane syringe;
+
+    public Syringe(){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/experiments/syringe/syringeFXML.fxml"));
+        loader.setRoot(this);
+        loader.setController(this);
+
+        try{
+            loader.load();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
