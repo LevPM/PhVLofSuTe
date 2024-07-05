75ecefaaa01ff0bd59614991d023efc65e14be77
LevPM <elkinlev2008@gmail.com>
2024-07-05 16:01:03 +0300

    Adding liquid visibility in LiquidContainer

--- src/main/java/ru/riddle/phVLofSuTe/model/experiments/syringe/LiquidContainer.java
+++ src/main/java/ru/riddle/phVLofSuTe/model/experiments/syringe/LiquidContainer.java
@@ -1,12 +1,22 @@
 package ru.riddle.phVLofSuTe.model.experiments.syringe;
 
+import javafx.fxml.FXML;
 import javafx.scene.Group;
+import javafx.scene.shape.Polygon;
+import ru.riddle.phVLofSuTe.model.experiments.Liquid;
 import ru.riddle.phVLofSuTe.model.util.FXMLs;
 import ru.riddle.phVLofSuTe.model.util.ModelUtil;
 
 public class LiquidContainer extends Group {
 
-    public LiquidContainer(){
+    private Liquid liquidType;
+
+    @FXML
+    private Polygon liquid;
+
+    public LiquidContainer(Liquid liquidType){
         ModelUtil.downloadCustomComponentFXML(FXMLs.LIQUID_CONTAINER, this);
+        this.liquidType = liquidType;
+        liquid.setFill(liquidType.getColor());
     }
 }
