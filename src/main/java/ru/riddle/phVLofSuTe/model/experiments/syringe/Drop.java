82570f5ea21d87a874822d325dca8857e990c705
LevPM <elkinlev2008@gmail.com>
2024-07-06 16:44:46 +0300

    Adding drop coloring

--- src/main/java/ru/riddle/phVLofSuTe/model/experiments/syringe/Drop.java
+++ src/main/java/ru/riddle/phVLofSuTe/model/experiments/syringe/Drop.java
@@ -2,6 +2,8 @@ package ru.riddle.phVLofSuTe.model.experiments.syringe;
 
 import javafx.animation.Transition;
 import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.scene.shape.Arc;
 import javafx.util.Duration;
 import ru.riddle.phVLofSuTe.model.util.FXMLs;
 import ru.riddle.phVLofSuTe.model.util.LiquidContainer;
@@ -9,8 +11,16 @@ import ru.riddle.phVLofSuTe.model.util.ModelUtil;
 
 public class Drop extends LiquidContainer {
 
+    @FXML
+    private Arc dropTip;
+
+    @FXML
+    private Arc dropCircle;
+
     public Drop(){
         ModelUtil.downloadCustomComponentFXML(FXMLs.DROP, this);
+        dropTip.setFill(this.getLiquidType().getColor());
+        dropCircle.setFill(this.getLiquidType().getColor());
     }
 
     public Transition getTransition(Duration entireDuration){
