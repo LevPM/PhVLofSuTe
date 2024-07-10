afb795ac7a21edeecb7f5bf5232edfafcf325386
LevPM <elkinlev2008@gmail.com>
2024-07-10 20:20:49 +0300

    Creating LiquidSettingListCell class

---
+++ src/main/java/ru/riddle/phVLofSuTe/model/experiments/thirdExperiment/LiquidSettingListCell.java
@@ -0,0 +1,13 @@
+package ru.riddle.phVLofSuTe.model.experiments.thirdExperiment;
+
+import javafx.scene.control.ListCell;
+import ru.riddle.phVLofSuTe.model.experiments.Liquid;
+import ru.riddle.phVLofSuTe.model.util.FXMLs;
+import ru.riddle.phVLofSuTe.model.util.ModelUtil;
+
+public class LiquidSettingListCell extends ListCell<Liquid> {
+
+    public LiquidSettingListCell(){
+        ModelUtil.downloadCustomComponentFXML(FXMLs.LIQUID_SETTING, this);
+    }
+}
