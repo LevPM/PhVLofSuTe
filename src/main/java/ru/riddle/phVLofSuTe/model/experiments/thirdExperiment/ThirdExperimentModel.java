c34b4f405b59e3a1c3e0134e7d1ed2c98e83e96f
LevPM <elkinlev2008@gmail.com>
2024-07-10 21:05:59 +0300

    Removing unused getLiquidCellFactory method

--- src/main/java/ru/riddle/phVLofSuTe/model/experiments/thirdExperiment/ThirdExperimentModel.java
+++ src/main/java/ru/riddle/phVLofSuTe/model/experiments/thirdExperiment/ThirdExperimentModel.java
@@ -1,10 +1,6 @@
 package ru.riddle.phVLofSuTe.model.experiments.thirdExperiment;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
-import javafx.util.Callback;
-import ru.riddle.phVLofSuTe.model.experiments.Liquid;
 import ru.riddle.phVLofSuTe.model.util.FXMLs;
 import ru.riddle.phVLofSuTe.model.util.ModelUtil;
 
@@ -15,9 +11,5 @@ public class ThirdExperimentModel {
 
     public void leaveExperiment(ActionEvent event){
         ModelUtil.changeScreen(event, FXMLs.EXPERIMENTS_MENU);
-    }
-
-    public Callback<ListView<Liquid>, ListCell<Liquid>> getLiquidCellFactory(){
-        return null;
     }
 }
