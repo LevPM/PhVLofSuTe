d5dc110894f7a4a577f0caa24af1fa13db586889
LevPM <elkinlev2008@gmail.com>
2024-07-10 22:04:33 +0300

    Adding initializeLiquidComboBox method and adding him calling to initialize method

--- src/main/java/ru/riddle/phVLofSuTe/controller/ThirdExperimentController.java
+++ src/main/java/ru/riddle/phVLofSuTe/controller/ThirdExperimentController.java
@@ -4,6 +4,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+import ru.riddle.phVLofSuTe.model.experiments.thirdExperiment.LiquidSettingListCell;
 import ru.riddle.phVLofSuTe.model.experiments.thirdExperiment.ThirdExperimentModel;
 import ru.riddle.phVLofSuTe.model.experiments.Liquid;
 import ru.riddle.phVLofSuTe.model.animation.Drop;
@@ -45,5 +49,16 @@ public class ThirdExperimentController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         this.model = new ThirdExperimentModel();
+        initializeLiquidComboBox(liquidComboBox);
+    }
+
+    private void initializeLiquidComboBox(ComboBox<Liquid> liquidComboBox){
+        liquidComboBox.getItems().addAll(Liquid.values());
+        liquidComboBox.setValue(Liquid.WATER);
+
+        Callback<ListView<Liquid>, ListCell<Liquid>> cellFactory = lv -> new LiquidSettingListCell();
+
+        liquidComboBox.setCellFactory(cellFactory);
+        liquidComboBox.setButtonCell(cellFactory.call(null));
     }
 }
