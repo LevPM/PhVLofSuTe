712410dcc11fcd055e9ad1efec9f4f244d7cce58
LevPM <elkinlev2008@gmail.com>
2024-07-11 15:24:09 +0300

    Creating Segmentable interface It should encapsulate segmentable components abstract functionality

---
+++ src/main/java/ru/riddle/phVLofSuTe/model/customComponents/Segmentable.java
@@ -0,0 +1,14 @@
+package ru.riddle.phVLofSuTe.model.customComponents;
+
+import javafx.beans.property.IntegerProperty;
+
+public interface Segmentable{
+
+    int getCountOfSegments();
+
+    void setCountOfSegments(int countOfSegments);
+
+    IntegerProperty countOfSegmentsProperty();
+    
+    void build();
+}
