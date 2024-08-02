9f5a277bf47213c5ba62ec26bf322b6e2c2f1705
LevPM <elkinlev2008@gmail.com>
2024-08-02 23:20:23 +0300

    Adding stub icon of app

--- src/main/java/ru/riddle/phVLofSuTe/PhVLofSuTe.java
+++ src/main/java/ru/riddle/phVLofSuTe/PhVLofSuTe.java
@@ -3,6 +3,7 @@ package ru.riddle.phVLofSuTe;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import ru.riddle.phVLofSuTe.model.util.FXMLs;
 import ru.riddle.phVLofSuTe.model.util.ModelUtil;
@@ -16,6 +17,7 @@ public class PhVLofSuTe extends Application {
         primaryStage.setScene(scene);
         primaryStage.setTitle("PhVLofSuTe");
         primaryStage.setResizable(false);
+        primaryStage.getIcons().add(new Image(getClass().getResource("/view/images/blackboard_icon.png").toExternalForm()));
         primaryStage.show();
     }
 
