354f59f06269c24eaa49a22c0a4638f00bb0072b
LevPM <elkinlev2008@gmail.com>
2024-08-08 16:36:57 +0300

    Changing LiquidTank extending from Group to Control

--- src/main/java/ru/riddle/phVLofSuTe/model/customComponents/LiquidTank.java
+++ src/main/java/ru/riddle/phVLofSuTe/model/customComponents/LiquidTank.java
@@ -2,14 +2,14 @@ package ru.riddle.phVLofSuTe.model.customComponents;
 
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.scene.Group;
+import javafx.scene.control.Control;
 import javafx.scene.shape.Shape;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public abstract class LiquidTank extends Group {
+public abstract class LiquidTank extends Control {
 
     private static final Liquid DEFAULT_LIQUID = Liquid.WATER;
 
