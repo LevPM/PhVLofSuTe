a32e12f7508749b4dd61e245399ee17109e8c6bf
LevPM <elkinlev2008@gmail.com>
2024-08-12 23:51:09 +0300

    Adding logging to PhVLofSuTe

--- src/main/java/ru/riddle/phVLofSuTe/PhVLofSuTe.java
+++ src/main/java/ru/riddle/phVLofSuTe/PhVLofSuTe.java
@@ -5,29 +5,37 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.riddle.phVLofSuTe.model.util.AppProperties;
 import ru.riddle.phVLofSuTe.model.util.FXMLs;
 import ru.riddle.phVLofSuTe.model.util.ModelUtil;
 
 public class PhVLofSuTe extends Application {
 
+    private static final Logger logger = LoggerFactory.getLogger(PhVLofSuTe.class);
+
     @Override
     public void start(Stage primaryStage){
+        logger.info("Starting application");
         Parent root = ModelUtil.downloadRoot(FXMLs.MENU_SCREEN);
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.setTitle("PhVLofSuTe");
         primaryStage.setResizable(false);
         primaryStage.getIcons().add(new Image(getClass().getResource("/view/images/blackboard_icon.png").toExternalForm()));
+        logger.info("Showing stage");
         primaryStage.show();
     }
 
     @Override
     public void init(){
+        logger.info("Initializing AppProperties");
         AppProperties.init();
     }
 
     public static void main(String[] args) {
+        logger.info("Launching application...");
         launch(args);
     }
 }
