d9a779f138044031e00842fd739937fae330f0bd
LevPM <elkinlev2008@gmail.com>
2024-08-13 16:38:07 +0300

    Correcting View Management system

--- src/main/java/ru/riddle/phVLofSuTe/model/ViewManager.java
+++ src/main/java/ru/riddle/phVLofSuTe/model/ViewManager.java
@@ -8,8 +8,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.riddle.phVLofSuTe.view.menu.AuthorsView;
-import ru.riddle.phVLofSuTe.view.menu.MenuView;
 
 public class ViewManager {
 
@@ -26,15 +24,6 @@ public class ViewManager {
 
     private static Parent downloadRoot(ViewName view){
         logger.debug("Loading view: {}", view.name());
-        return FluentViewLoader.fxmlView(
-                switch (view){
-                    case MENU -> MenuView.class;
-                    case AUTHORS -> AuthorsView.class;
-                    default -> {
-                        logger.error("View not found! View name: {}", view.name());
-                        yield null;
-                    }
-                })
-                .load().getView();
+        return FluentViewLoader.fxmlView(view.getValue()).load().getView();
     }
 }
--- src/main/java/ru/riddle/phVLofSuTe/model/ViewName.java
+++ src/main/java/ru/riddle/phVLofSuTe/model/ViewName.java
@@ -1,11 +1,31 @@
 package ru.riddle.phVLofSuTe.model;
 
+import de.saxsys.mvvmfx.FxmlView;
+import de.saxsys.mvvmfx.ViewModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.riddle.phVLofSuTe.view.menu.AuthorsView;
+import ru.riddle.phVLofSuTe.view.menu.MenuView;
+
 public enum ViewName {
 
-    MENU,
-    MENU_TE,
-    AUTHORS,
-    SETTINGS,
+    MENU(MenuView.class),
+    MENU_TE(null),
+    AUTHORS(AuthorsView.class),
+    SETTINGS(null),
+    ;
 
+    private final Class<? extends FxmlView<? extends ViewModel>> value;
+
+    private static final Logger logger = LoggerFactory.getLogger(ViewName.class);
+
+    ViewName(Class<? extends FxmlView<? extends ViewModel>> value){
+        this.value = value;
+    }
+
+    public Class<? extends FxmlView<? extends ViewModel>> getValue() {
+        logger.trace("Returning ViewName value: {}", value.getName());
+        return value;
+    }
 
 }
