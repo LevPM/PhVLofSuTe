7cb543e8a6df5976df4cb339531bca1a66c362ad
LevPM <elkinlev2008@gmail.com>
2024-08-17 20:39:01 +0300

    Adding variables to BasicExampleView

--- src/main/java/ru/riddle/phVLofSuTe/view/theory/examples/BasicExampleView.java
+++ src/main/java/ru/riddle/phVLofSuTe/view/theory/examples/BasicExampleView.java
@@ -3,7 +3,9 @@ package ru.riddle.phVLofSuTe.view.theory.examples;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.Pagination;
+import javafx.scene.image.ImageView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import de.saxsys.mvvmfx.FxmlView;
@@ -16,6 +18,12 @@ import java.util.ResourceBundle;
 public class BasicExampleView implements FxmlView<BasicExampleViewModel>, Initializable {
 
     private static final Logger logger = LoggerFactory.getLogger(BasicExampleView.class);
+
+    @FXML
+    private Label exampleNumberLabel;
+
+    @FXML
+    private ImageView exampleImage;
 
     @FXML
     private Pagination pagination;
