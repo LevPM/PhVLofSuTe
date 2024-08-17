12d8b5c8adda128cc60973a836ad8959e5cd4c64
LevPM <elkinlev2008@gmail.com>
2024-08-17 20:34:14 +0300

    Removing unused import

--- src/main/java/ru/riddle/phVLofSuTe/viewModel/theory/examples/TheoryExamplesSelectionViewModel.java
+++ src/main/java/ru/riddle/phVLofSuTe/viewModel/theory/examples/TheoryExamplesSelectionViewModel.java
@@ -1,7 +1,6 @@
 package ru.riddle.phVLofSuTe.viewModel.theory.examples;
 
 import javafx.event.ActionEvent;
-import javafx.scene.Node;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import de.saxsys.mvvmfx.ViewModel;
