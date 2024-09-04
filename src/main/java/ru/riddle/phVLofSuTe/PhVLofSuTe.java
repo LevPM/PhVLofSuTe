3daf4be061caf00852a00f30e44aadf36d57acd1
Elkin Lev <elkinlev2008@gmail.com>
2024-09-05 00:18:13 +0300

    Removing unused import statement

--- src/main/java/ru/riddle/phVLofSuTe/PhVLofSuTe.java
+++ src/main/java/ru/riddle/phVLofSuTe/PhVLofSuTe.java
@@ -11,7 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.riddle.phVLofSuTe.model.AppProperties;
 import ru.riddle.phVLofSuTe.model.data.ImageDataManager;
-import ru.riddle.phVLofSuTe.view.customComponents.Syringe;
 import ru.riddle.phVLofSuTe.view.menu.MenuView;
 import ru.riddle.phVLofSuTe.viewModel.menu.MenuViewModel;
 
