d0ee926387f21968d95b82a0e5483e3fc75bede6
Elkin Lev <elkinlev2008@gmail.com>
2025-03-09 22:28:30 +0300

    Creating first version of Task generator

--- src/main/java/ru/riddle/phVLofSuTe/model/tasks/TasksGenerator.java
+++ src/main/java/ru/riddle/phVLofSuTe/model/tasks/TasksGenerator.java
@@ -7,15 +7,24 @@ import ru.riddle.phVLofSuTe.viewModel.tasks.GeneratedTaskDataScope;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Random;
 
 public class TasksGenerator {
 
     private GeneratedTaskDataScope generateTask(){
         Task task = JSONDataManager.getById((int)(Math.random() * 5) + 1, Task.class);
         String condition = Objects.requireNonNull(task).condition();
-        String resCondition = condition;
         List<String> vars = task.vars();
-        return null;
+        List<Variable> variables = parseVars(vars);
+        for(Variable variable: variables){
+            Random r = new Random();
+            double rangeMin = variable.valueFrom;
+            double rangeMax = variable.valueTo;
+            double randomValue = rangeMin + (rangeMax - rangeMin) * r.nextDouble();
+
+            condition = condition.replace(variable.name, Double.toString(randomValue));
+        }
+        return new GeneratedTaskDataScope(task.level(), condition);
     }
 
     public List<GeneratedTaskDataScope> generateTasks(int count){
@@ -25,4 +34,24 @@ public class TasksGenerator {
         }
         return result;
     }
+
+    private Variable parseVar(String variable){
+        return new Variable(
+                variable.substring(0, 1),
+                Double.parseDouble(variable.substring(2, variable.indexOf("-"))),
+                Double.parseDouble(variable.substring(variable.indexOf("-") + 1, variable.indexOf(" "))),
+                variable.substring(variable.indexOf(" ") + 1)
+        );
+    }
+
+    private List<Variable> parseVars(List<String> variables){
+        return variables.stream().map(this::parseVar).toList();
+    }
+
+    record Variable(
+            String name,
+            double valueFrom,
+            double valueTo,
+            String unit
+    ){}
 }
